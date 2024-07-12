91a8dd27a196e55b4a4caf8c1c982751b3196d13
longle325 <lebaolong74@gmail.com>
2024-07-12 09:35:06 +0000

    Debug commit

--- frontend/js/document.js
+++ frontend/js/document.js
@@ -1,5 +1,4 @@
-import {post} from "./request.js"
-// let palettes = document.getElementsByClassName("palette");
+import { post } from "./request.js";
 let canvasBlock0 = document.getElementById("canvasBlock0");
 
 let query0 = "";
@@ -11,155 +10,185 @@ let objects1 = [];
 let pos0 = [];
 let pos1 = [];
 
-const desiredWidth = 30; 
-const desiredHeight = 30; 
+const desiredWidth = 30;
+const desiredHeight = 30;
 
 function getData() {
-    let res = {query: [{textual: query0, objects: objects0.join(' '), txt: convertPosToStr(pos0)},{textual: query1, objects: objects1.join(' '), txt: convertPosToStr(pos1)}]}
-    res.query = res.query.map(obj => {
-        let filteredObject = {};
-        for (let key in obj)
-            if (obj[key])
-                filteredObject[key] = obj[key];
-        return filteredObject;
-    });
+  let res = {
+    query: [
+      {
+        textual: query0,
+        objects: objects0.join(" "),
+        txt: convertPosToStr(pos0),
+      },
+      {
+        textual: query1,
+        objects: objects1.join(" "),
+        txt: convertPosToStr(pos1),
+      },
+    ],
+  };
+  res.query = res.query.map((obj) => {
+    let filteredObject = {};
+    for (let key in obj) if (obj[key]) filteredObject[key] = obj[key];
+    return filteredObject;
+  });
 
-    res.query = res.query.filter(e => Object.keys(e).length);
-    return JSON.stringify(res);
+  res.query = res.query.filter((e) => Object.keys(e).length);
+  return JSON.stringify(res);
 }
 
 function convertPosToStr(pos) {
-    let res = [];
-    pos.forEach(element => {
-        res.push(element.join(' '));
-    });
-    return res.join(' ');
+  let res = [];
+  pos.forEach((element) => {
+    res.push(element.join(" "));
+  });
+  return res.join(" ");
 }
 
 function calcGridPos(x, y, name) {
-    let size = 13;
-    let cellWidth = canvasBlock0.clientWidth/size;
-    let cellHeight = canvasBlock0.clientHeight/size;
+  let size = 13;
+  let cellWidth = canvasBlock0.clientWidth / size;
+  let cellHeight = canvasBlock0.clientHeight / size;
 
-    let startX = Math.max(0, Math.floor(x/cellWidth));
-    let startY = Math.max(0, Math.floor(y/cellHeight));
+  let startX = Math.max(0, Math.floor(x / cellWidth));
+  let startY = Math.max(0, Math.floor(y / cellHeight));
 
-    let endX = Math.min(size, Math.ceil((x+desiredWidth)/cellWidth));
-    let endY = Math.min(size, Math.ceil((y+desiredHeight)/cellHeight));
+  let endX = Math.min(size, Math.ceil((x + desiredWidth) / cellWidth));
+  let endY = Math.min(size, Math.ceil((y + desiredHeight) / cellHeight));
 
-    let res = [];
-    for (let i=startX; i < endX; i++) {
-        for (let j=startY; j < endY; j++) {
-            let temp = String(i)+String.fromCharCode(97+j)+name;
-            res.push(temp);
-        }
+  let res = [];
+  for (let i = startX; i < endX; i++) {
+    for (let j = startY; j < endY; j++) {
+      let temp = String(i) + String.fromCharCode(97 + j) + name;
+      res.push(temp);
     }
-    return res;
+  }
+  return res;
 }
 
 // Add event listeners for canvasBlock0
 canvasBlock0.addEventListener("dragover", function (e) {
-    e.preventDefault();
+  e.preventDefault();
 });
 
 canvasBlock0.addEventListener("drop", function (e) {
-    e.preventDefault();
-    
-    let formData = new FormData(document.getElementById("request"));
-    query0 = formData.get("scene_description");
-    query1 = formData.get("next_scene_description");
+  e.preventDefault();
 
-    let data = e.dataTransfer.getData("text");
-    if(!objects0.includes(data)) {
-        objects0.push(data);
+  let formData = new FormData(document.getElementById("request"));
+  query0 = formData.get("scene_description");
+  query1 = formData.get("next_scene_description");
+
+  let data = e.dataTransfer.getData("text");
+  if (!objects0.includes(data)) {
+    objects0.push(data);
+  }
+
+  let draggedElement = document.getElementById(data);
+
+  // Ensure the dropped element is an <img> element
+  if (draggedElement.tagName.toLowerCase() === "img") {
+    // Append the dragged element to the drop zone
+    canvasBlock0.appendChild(draggedElement);
+
+    // Optionally, position the dropped element within the drop zone
+    let rect = canvasBlock1.getBoundingClientRect();
+    let x = e.clientX - rect.left - desiredWidth / 2;
+    let y = e.clientY - rect.top - desiredHeight / 2;
+    draggedElement.style.position = "absolute";
+    draggedElement.style.left = x + "px";
+    draggedElement.style.top = y + "px";
+    draggedElement.style.width = desiredWidth + "px";
+    draggedElement.style.height = desiredHeight + "px";
+
+    console.log(objects0);
+    if (objects0.includes(data)) {
+      let index = objects0.indexOf(data);
+      pos0[index] = calcGridPos(
+        e.clientX - canvas0.getBoundingClientRect().x,
+        e.clientY - canvas0.getBoundingClientRect().y,
+        data
+      );
+    } else {
+      pos0.push(
+        calcGridPos(
+          e.clientX - canvas0.getBoundingClientRect().x,
+          e.clientY - canvas0.getBoundingClientRect().y,
+          data
+        )
+      );
     }
+    post(getData());
 
-    let draggedElement = document.getElementById(data);
-
-    // Ensure the dropped element is an <img> element
-    if (draggedElement.tagName.toLowerCase() === 'img') {
-        // Append the dragged element to the drop zone
-        canvasBlock1.appendChild(draggedElement);
-
-        // Optionally, position the dropped element within the drop zone
-        let rect = canvasBlock1.getBoundingClientRect();
-        let x = e.clientX - rect.left - desiredWidth/2;
-        let y = e.clientY - rect.top - desiredHeight/2;
-        draggedElement.style.position = "absolute";
-        draggedElement.style.left = x + "px";
-        draggedElement.style.top = y + "px";
-        draggedElement.style.width = desiredWidth + "px";
-        draggedElement.style.height = desiredHeight + "px";
-
-        console.log(objects0);
-        if(objects0.includes(data)) {
-            let index = objects0.indexOf(data);
-            pos0[index] = calcGridPos(e.clientX-canvas0.getBoundingClientRect().x, e.clientY-canvas0.getBoundingClientRect().y, data);
-        } else {
-            pos0.push(calcGridPos(e.clientX-canvas0.getBoundingClientRect().x, e.clientY-canvas0.getBoundingClientRect().y, data));
-        }
-        post(getData())
-
-        console.log(getData());
-    }
+    console.log(getData());
+  }
 });
 
 let canvasBlock1 = document.getElementById("canvasBlock1");
 
 // Add event listeners for canvasBlock1
 canvasBlock1.addEventListener("dragover", function (e) {
-    e.preventDefault();
+  e.preventDefault();
 });
 
 canvasBlock1.addEventListener("drop", function (e) {
-    e.preventDefault();
-    
-    let formData = new FormData(document.getElementById("request"));
-    query0 = formData.get("scene_description");
-    query1 = formData.get("next_scene_description");
+  e.preventDefault();
 
-    let data = e.dataTransfer.getData("text");
-    if(!objects1.includes(data)) {
-        objects1.push(data);
+  let formData = new FormData(document.getElementById("request"));
+  query0 = formData.get("scene_description");
+  query1 = formData.get("next_scene_description");
+
+  let data = e.dataTransfer.getData("text");
+  if (!objects1.includes(data)) {
+    objects1.push(data);
+  }
+
+  let draggedElement = document.getElementById(data);
+
+  // Ensure the dropped element is an <img> element
+  if (draggedElement.tagName.toLowerCase() === "img") {
+    // Append the dragged element to the drop zone
+    canvasBlock1.appendChild(draggedElement);
+
+    // Optionally, position the dropped element within the drop zone
+    let rect = canvasBlock1.getBoundingClientRect();
+    let x = e.clientX - rect.left - desiredWidth / 2;
+    let y = e.clientY - rect.top - desiredHeight / 2;
+    draggedElement.style.position = "absolute";
+    draggedElement.style.left = x + "px";
+    draggedElement.style.top = y + "px";
+    draggedElement.style.width = desiredWidth + "px";
+    draggedElement.style.height = desiredHeight + "px";
+
+    console.log(objects1);
+    if (objects1.includes(data)) {
+      let index = objects1.indexOf(data);
+      pos1[index] = calcGridPos(
+        e.clientX - canvas1.getBoundingClientRect().x,
+        e.clientY - canvas1.getBoundingClientRect().y,
+        data
+      );
+    } else {
+      pos1.push(
+        calcGridPos(
+          e.clientX - canvas1.getBoundingClientRect().x,
+          e.clientY - canvas1.getBoundingClientRect().y,
+          data
+        )
+      );
     }
-
-    let draggedElement = document.getElementById(data);
-
-    // Ensure the dropped element is an <img> element
-    if (draggedElement.tagName.toLowerCase() === 'img') {
-        // Append the dragged element to the drop zone
-        canvasBlock1.appendChild(draggedElement);
-
-        // Optionally, position the dropped element within the drop zone
-        let rect = canvasBlock1.getBoundingClientRect();
-        let x = e.clientX - rect.left - desiredWidth/2;
-        let y = e.clientY - rect.top - desiredHeight/2;
-        draggedElement.style.position = "absolute";
-        draggedElement.style.left = x + "px";
-        draggedElement.style.top = y + "px";
-        draggedElement.style.width = desiredWidth + "px";
-        draggedElement.style.height = desiredHeight + "px";
-
-        console.log(objects1);
-        if(objects1.includes(data)) {
-            let index = objects1.indexOf(data);
-            pos1[index] = calcGridPos(e.clientX-canvas1.getBoundingClientRect().x, e.clientY-canvas1.getBoundingClientRect().y, data);
-        } else {
-            pos1.push(calcGridPos(e.clientX-canvas1.getBoundingClientRect().x, e.clientY-canvas1.getBoundingClientRect().y, data));
-        }
-        post(getData())
-
-        console.log(getData());
-    }
-});
-
-document.getElementById("request").addEventListener("submit", function (e) {
-    e.preventDefault();
-    let formData = new FormData(e.target);
-    query0 = formData.get("scene_description");
-    query1 = formData.get("next_scene_description");
-    post(getData())
+    post(getData());
 
     console.log(getData());
+  }
 });
 
+// document.getElementById("request").addEventListener("submit", function (e) {
+//     e.preventDefault();
+//     let formData = new FormData(e.target);
+//     query0 = formData.get("scene_description");
+//     query1 = formData.get("next_scene_description");
+//     post(getData())
+
+//     console.log(getData());
+// });
--- frontend/js/request.js
+++ frontend/js/request.js
@@ -1,20 +1,22 @@
 export function post(data) {
-    fetch('http://localhost:8053/', {
-        method: 'POST',
-        headers: {
-            'accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: data
-    }).then(res => {
-        if (res.ok) {
-            console.log("SUCCESS");
-            res.json().then(response => console.log(response));
-        } else {
-            console.log("Not successful");
-            console.log("Status: " + res.status);
-            console.log("Status Text: " + res.statusText);
-            res.text().then(text => console.log("Response Body: " + text));
-        }
-    }).catch(error => console.log("Fetch error: ", error));
+  fetch("http://localhost:8053/", {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: data, 
+  })
+    .then((res) => {
+      if (res.ok) {
+        console.log("SUCCESS");
+        res.json().then((response) => console.log(response));
+      } else {
+        console.log("Not successful");
+        console.log("Status: " + res.status);
+        console.log("Status Text: " + res.statusText);
+        res.text().then((text) => console.log("Response Body: " + text));
+      }
+    })
+    .catch((error) => console.log("Fetch error: ", error));
 }
